ebcc8322703efad8fc4bf7df74b8fcbb761ec99b
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2020-09-23 14:18:36 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.use(express.static(__dirname))
-
-app.listen(process.env.PORT, (err) => {
-  console.log('Running on port ' + process.env.PORT)
-})
